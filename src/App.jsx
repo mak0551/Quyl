a97f94846d08bcfe79db7f30f12e4565ed4b6d4e
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-03-19 20:12:19 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,15 @@ import React, { useEffect, useState } from "react";
 import StudentDashboard from "./components/dashboard/Dashboard.jsx";
 import Signup from "./pages/Signup.jsx";
 import { Signin } from "./pages/Signin.jsx";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import { firebaseAuth } from "./context/Firebase.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [user, setUser] = useState(null);
-  // useEffect(() => {
-  //   onAuthStateChanged(firebaseAuth, (user) => {
-  //     if (user) {
-  //       console.log(user, "user signed in");
-  //       setUser(user);
-  //     } else {
-  //       navigate("/signin");
-  //       setUser(null);
-  //     }
-  //   });
-  // }, []);
-
   const signout = () => {
     localStorage.removeItem("user");
     signOut(firebaseAuth);
   };
-
-  // if (user === null) {
-  //   return (
-  //     <div className="flex justify-center items-center flex-col gap-8">
-  //       <Signup />
-  //       <Signin />
-  //     </div>
-  //   );
-  // }
 
   return (
     <div className="flex justify-center items-center flex-col gap-8">
@@ -43,9 +21,8 @@ function App() {
           <Route path="/" element={<StudentDashboard />} />
         </Routes>
       </Router>
-      {/* <StudentDashboard /> */}
-      {/* <h1>hello user.email</h1>
-      <button onClick={signout}>logout</button> */}
+
+      {/* <button onClick={signout}>logout</button>  */}
     </div>
   );
 }
